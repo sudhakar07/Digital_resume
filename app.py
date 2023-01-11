3460acc60912e9fc16dd88516c17d58151b514d2
sudhakar07 <sudhakargk74@gmail.com>
2023-01-12 00:24:39 +0530

    Update app.py
    contact form added

--- app.py
+++ app.py
@@ -192,3 +192,31 @@ st.info(
                                                                    \n
 """
 )
+
+
+st.write('\n')
+
+
+st.header(":mailbox: Get In Touch With Me!")
+st.write("---")
+
+
+contact_form = """
+<form action="https://formsubmit.co/94bdffa1516793e9eeb6b6010eaa07cb" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("styles/style.css")
